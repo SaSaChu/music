14307d99d53261d54cd5cdd3ad0051f7261aebfc
SaSaChu <teresa7397@gmail.com>
2016-09-19 21:41:50 +0800

    Tmp

---
+++ js/public.js
@@ -0,0 +1,41 @@
+/**
+ * @author      ZEUS Design - http://www.zeusdesign.com.tw
+ * @copyright   Copyright (c) 2016 ZEUS Design
+ */
+
+$(function () {
+  if ($(window).width () < 1000) {
+    var subs = [];
+
+    var arr = $('.top_down .top_down_cent > div').map (function () {
+      var $a = $(this).find ('> a').eq (0);
+      var $as = $a.nextAll ();
+
+      if ($as.length > 2) {
+
+        subs = subs.concat ($as.map (function () {
+            return {
+                href: $(this).attr ('href'),
+                text: $(this).text ()
+              };
+          }).toArray ());
+        return null;
+      } else {
+        return [{
+            href: $a.attr ('href'),
+            text: $a.text ()
+          }];
+      }
+      return re;
+    }).filter (function (t) { return t != null; }).toArray ();
+
+    arr = arr.concat (subs);
+
+    $('<div />').addClass ('top_down_m').append (
+      $('<select / >').append (arr.map (function (t) {
+        return $('<option />').text (t.text).val (t.href);
+      })).change (function () {
+        if ($(this).val ()) window.location = $(this).val ();
+      })).insertBefore ($('.top_down'));
+  }
+});
